58c1f57ca3b67c4eef87e137c386b37f95fd7f5a
maksymyurchak <wukarenF@gmail.com>
2018-04-28 15:05:44 +0300

    Init commit

---
+++ Invoice.API/Controllers/CustomerController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Invoice.API.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/Customer")]
+    public class CustomerController : Controller
+    {
+        /// <summary>
+        /// Get all customers
+        /// </summary>
+        [HttpGet]
+        public void GetAllCustomers()
+        {
+
+        }
+        [HttpPost]
+        public void AddCustomer()
+        {
+
+        }
+    }
+}
---
+++ Models/Enum/TypeProduct.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models.Enum
+{
+    public enum TypeProduct
+    {
+        Kilogram = 1,
+        Artificially = 2
+    }
+}
